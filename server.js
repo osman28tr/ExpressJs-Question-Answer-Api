dd685ce171f3e6f3b9809d9ceb2af9d0d3460343
osman28tr <osmantonbul2001@gmail.com>
2023-08-30 15:02:36 +0300

    created project and configures

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//Environment variables
+dotenv.config({
+    path:"./config/env/config.env"
+});
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/",(req,res)=>{
+    res.send("Hello question answer api");
+})
+
+app.listen(PORT,()=>{
+    console.log(`App started on ${process.env.PORT} : ${process.env.NODE_ENV}`);
+});
