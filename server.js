b6da345180c9e8a0f11f35fd3758929d8529e756
osman28tr <osmantonbul2001@gmail.com>
2023-08-30 17:32:43 +0300

    mongodb connected done

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const routers = require('./routers/index');
+const connectDatabase = require('./helpers/database/connectDatabase');
 
 //Environment variables
 dotenv.config({
     path:"./config/env/config.env"
 });
+
+//Mongodb connection
+connectDatabase();
+
 const app = express();
 const PORT = process.env.PORT;
 
