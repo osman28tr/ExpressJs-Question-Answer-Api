b0b073ea84c0815fadc80d1520df4855d0f9f91d
osman28tr <osmantonbul2001@gmail.com>
2023-08-30 15:07:19 +0300

    deneme

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get("/",(req,res)=>{
     res.send("Hello question answer api");
 })
 
+app.get("/test",(req,res)=>{
+    res.send("testing");
+})
+
 app.listen(PORT,()=>{
     console.log(`App started on ${process.env.PORT} : ${process.env.NODE_ENV}`);
 });
