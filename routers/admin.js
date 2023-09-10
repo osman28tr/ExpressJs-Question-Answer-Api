be3f35bf5f960094edc2b5c1bb2aab971637af7a
osman28tr <osmantonbul2001@gmail.com>
2023-09-10 17:10:05 +0300

    changed user block status

--- routers/admin.js
+++ routers/admin.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {getAccessToRoute,getAdminAccess} = require('../middlewares/authorization/auth');
+const {checkUserExist} = require('../middlewares/database/databaseErrorHelpers');
 //block and delete users
+const {blockUser} = require('../controllers/admin');
 router.use([getAccessToRoute,getAdminAccess]);
-router.get("/",(req,res,next)=>{
-    res.status(200)
-    .json({
-        status:true,
-        message:"test"
-    });
-});
+
+router.get("/block/:id",checkUserExist,blockUser);
 module.exports = router;
